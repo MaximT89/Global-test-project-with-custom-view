6ef1349f595c314c202fc9d7bb23ad80ed9255d8
BuenasDias <tmaksim1989@gmail.com>
2022-12-06 09:43:31 +0300

    create first custom view

---
+++ app/src/main/java/com/max/secondworld/globaltestprojectwithcustomview/ui/custom_view/RingView.kt
@@ -0,0 +1,62 @@
+package com.max.secondworld.globaltestprojectwithcustomview.ui.custom_view
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
+import android.util.AttributeSet
+import android.view.View
+import com.max.secondworld.globaltestprojectwithcustomview.R
+import kotlin.math.min
+
+class RingView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+
+    var size = 320f
+    private var color : Int = resources.getColor(R.color.black)
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        size = min(measuredWidth, measuredHeight).toFloat()
+        setMeasuredDimension(size.toInt(), size.toInt())
+    }
+
+    fun setColorPath(newColor : Int) {
+        color = newColor
+        invalidate()
+    }
+
+    @SuppressLint("DrawAllocation")
+    override fun onDraw(canvas: Canvas?) {
+
+        val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        paint.color = color
+        paint.isAntiAlias = true
+        paint.style = Paint.Style.FILL_AND_STROKE
+        paint.strokeWidth = 10f
+
+        val path = Path()
+
+        path.reset()
+
+        path.moveTo(size / 2, 0f)
+        path.lineTo(size / 8 * 5, size / 8 * 3)
+        path.lineTo(size, size / 2)
+        path.lineTo(size / 8 * 5 , size / 8 * 5)
+        path.lineTo(size / 2 , size)
+        path.lineTo(size / 8 * 3 , size / 8 * 5)
+        path.lineTo(0f , size / 2)
+        path.lineTo(size / 8 * 3 , size / 8 * 3)
+        path.moveTo(size / 2, 0f)
+
+        canvas?.drawPath(path, paint)
+
+        super.onDraw(canvas)
+    }
+
+}
--- app/src/main/java/com/max/secondworld/globaltestprojectwithcustomview/ui/screens/first/FirstFragment.kt
+++ app/src/main/java/com/max/secondworld/globaltestprojectwithcustomview/ui/screens/first/FirstFragment.kt
@@ -1,13 +1,18 @@
 package com.max.secondworld.globaltestprojectwithcustomview.ui.screens.first
 
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
+import com.max.secondworld.globaltestprojectwithcustomview.R
 import com.max.secondworld.globaltestprojectwithcustomview.core.bases.BaseFragment
 import com.max.secondworld.globaltestprojectwithcustomview.databinding.FragmentFirstBinding
 
 class FirstFragment : BaseFragment<FragmentFirstBinding, FirstViewModel>(FragmentFirstBinding::inflate) {
     override val viewModel: FirstViewModel by viewModels()
 
-    override fun initView() {
+    override fun initView() = with(binding){
+
+        ring.setColorPath(ContextCompat.getColor(requireActivity(), R.color.alice_blue))
+
     }
 
     override fun initObservers() {
